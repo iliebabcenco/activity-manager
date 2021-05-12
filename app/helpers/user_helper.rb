fda909d483c2976a999498297fe6ccecc6fce6be
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-05-12 17:36:49 +0300

    Add styles for Sign up and root page

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,26 +1,26 @@
 module UserHelper
 
-    def display_external_activities_by_param(param="true", user)
-        if param == "true"
-            return user.external_activities.to_a
-        else
-            return user.external_previous_activities.to_a
-        end
-    end
-    def display_personal_activities(param="true", user)
-        if param == "true"
-            return user.personal_activities.to_a
-        else
-            return user.personal_previous_activities.to_a
-        end
-    end
-    def display_participation_activities(param="true", user)
+  def display_external_activities_by_param(param="true", user)
       if param == "true"
-          return user.all_participations.to_a
+          return user.external_activities.to_a
       else
-          return user.all_desc_participations.to_a
+          return user.external_previous_activities.to_a
       end
+  end
+  def display_personal_activities(param="true", user)
+      if param == "true"
+          return user.personal_activities.to_a
+      else
+          return user.personal_previous_activities.to_a
+      end
+  end
+  def display_participation_activities(param="true", user)
+    if param == "true"
+        return user.all_participations.to_a
+    else
+        return user.all_desc_participations.to_a
     end
+  end
 
     
   def display_user_page(user)
@@ -35,11 +35,7 @@ module UserHelper
       html += "<li>"+(link_to "All groups", groups_path)+"</li>"
       html += "<li>"+(link_to "Feed", feed_path)+"</li>"
       html += "<li>"+(link_to "Sign Out", destroy_user_session_path)+"</li></ul>"
-    else
-        html += "<h3>" + @user.username+"</h3>"
-      html += "<li>"+(link_to "External #{@user.username}'s activities", external_activities_path)+"</li>"
-      html += "<li>"+(link_to "All #{@user.username}'s participations", external_activities_path)+"</li>"
-      html += "<li>"+(link_to "All #{@user.username}'s groups", groups_path)+"</li></ul>"
+    
   
     end
     html.html_safe
