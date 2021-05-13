e987837882cd57cf19368d834757d457e0be9db7
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-05-13 16:08:16 +0300

    Add icon validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,7 @@
 class Group < ApplicationRecord
     has_many :activities, foreign_key: "group_id", class_name: 'Activity', dependent: :destroy
     belongs_to :creator, class_name: 'User'
-   
+    validates_format_of :icon, :with => %r{\.(gif|jpe?g|png)$}i, :message => "must have an image extension (jpg, jpe, png, gif)", :multiline => true
     validates :name, length: { in: 3..20 }
     validates :icon, presence: true
-    
 end
